88e30fbd547bd732217e56ab0a929b16306e4898
Sebastien REMY <seb.remy@me.com>
2019-11-04 18:27:27 +0100

    Add count me

--- app/src/main/java/com/sebremy/firstandroidappinkotlin/MainActivity.kt
+++ app/src/main/java/com/sebremy/firstandroidappinkotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sebremy.firstandroidappinkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +18,14 @@ class MainActivity : AppCompatActivity() {
         myToast.show()
     }
 
+    fun countMe(view: View) {
+        val showCountTextView = findViewById<TextView>(R.id.textView)
+
+        val countString = showCountTextView.text.toString()
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        showCountTextView.text = count.toString()
+    }
 
 }
